6764226f8c49d7a7fe2a58dfe3da321e631745b0
IvanAram <a01022584@itesm.mx>
2018-04-25 18:37:31 -0500

    Fixed movement

--- js/movement.js
+++ js/movement.js
@@ -3,19 +3,19 @@ function onKeyDown(event) {
   switch (event.keyCode) {
     case 38: // up
     case 87: // w
-      controls.getObject.translateZ(-1);
+      controls.getObject().translateZ(-1);
       break;
     case 37: // left
     case 65: // a
-      controls.getObject.translateX(-1);
+      controls.getObject().translateX(-1);
       break;
     case 40: // down
     case 83: // s
-      controls.getObject.translateZ(1);
+      controls.getObject().translateZ(1);
       break;
     case 39: // right
     case 68: // d
-      controls.getObject.translateX(1);
+      controls.getObject().translateX(1);
       break;
     case 74:
       fire();
