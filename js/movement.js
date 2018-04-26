3b4517e3a5d6ff0ff01ee63da714223c56920d12
José Manuel <josemanuel@guruweb.mx>
2018-04-26 13:21:45 -0500

    Fix fluid movementç

--- js/movement.js
+++ js/movement.js
@@ -3,22 +3,59 @@ function onKeyDown(event) {
   switch (event.keyCode) {
     case 38: // up
     case 87: // w
-      controls.getObject().translateZ(-1);
+      moveForward = true;
       break;
     case 37: // left
     case 65: // a
-      controls.getObject().translateX(-1);
+      moveLeft = true;
       break;
     case 40: // down
     case 83: // s
-      controls.getObject().translateZ(1);
+      moveBackward = true;
       break;
     case 39: // right
     case 68: // d
-      controls.getObject().translateX(1);
-      break;
-    case 74:
-      fire();
+      moveRight = true;
       break;
   }
+}
+
+function onKeyUp(event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      moveForward = false;
+      break;
+    case 37: // left
+    case 65: // a
+      moveLeft = false;
+      break;
+    case 40: // down
+    case 83: // s
+      moveBackward = false;
+      break;
+    case 39: // right
+    case 68: // d
+      moveRight = false;
+      break;
+  }
+}
+
+function handleMovement(delta) {
+  velocity.x -= velocity.x * 10.0 * delta;
+  velocity.z -= velocity.z * 10.0 * delta;
+
+  direction.z = Number(moveForward) - Number(moveBackward);
+  direction.x = Number(moveLeft) - Number(moveRight);
+  direction.normalize(); // this ensures consistent movements in all directions
+
+  if (moveForward || moveBackward)
+    velocity.z -= direction.z * 400.0 * delta;
+  if (moveLeft || moveRight)
+    velocity.x -= direction.x * 400.0 * delta;
+
+  controls.getObject().translateX(velocity.x * delta);
+  controls.getObject().translateZ(velocity.z * delta);
+
+  prevTime = time;
 }
